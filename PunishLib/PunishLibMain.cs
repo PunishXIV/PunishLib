8815bc04db296885378c9a94361b3b75dd9a59e5
Kane Valentine <kane@cute.im>
2023-01-09 16:43:46 +0000

    init repo

---
+++ PunishLib/PunishLibMain.cs
@@ -0,0 +1,23 @@
+﻿using Dalamud.Plugin;
+using ECommons;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PunishLib
+{
+    public class PunishLibMain
+    {
+        public static void Init(DalamudPluginInterface pluginInterface, IDalamudPlugin instance, params Module[] modules)
+        {
+            ECommonsMain.Init(pluginInterface, instance, modules);
+        }
+
+        public static void Dispose()
+        {
+            ECommonsMain.Dispose();
+        }
+    }
+}
