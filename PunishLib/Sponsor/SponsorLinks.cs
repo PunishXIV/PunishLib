40d2c5c460e33bda633c56921fd8e24adadc6e53
Limiana <5073202+Limiana@users.noreply.github.com>
2023-01-11 23:42:23 +0300

    Update sponsors

---
+++ PunishLib/Sponsor/SponsorLinks.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PunishLib.Sponsor
+{
+    public static class SponsorLinks
+    {
+        public static string Punish = "https://ko-fi.com/spetsnaz";
+    }
+}
